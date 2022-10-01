90454c2c227190ba32dd930a92ba95bd874d51b4
sitaram-mulik <sitaram99.race@gmail.com>
2022-10-01 12:24:45 +0530

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,11 @@ import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './AppRouter';
 import AppHeader from './components/header/AppHeader';
-import { httpClient } from './utilities';
 
 const App = () => {
-    // useEffect(() => {
-    //     httpClient
-    //         .post('/login', {
-    //             username: '+912222222222',
-    //             password: '222@222',
-    //             role: 'driver',
-    //         })
-    //         .then((d: any) => {
-    //             console.log('data ', d);
-    //             httpClient
-    //                 .get('/profile', {
-    //                     headers: {
-    //                         Authorization: `Bearer ${d.access_token}`,
-    //                     },
-    //                 })
-    //                 .then(
-    //                     (d) => {
-    //                         console.log('profile ', d);
-    //                     },
-    //                     (err) => {
-    //                         console.log('err ', err);
-    //                     },
-    //                 );
-    //         });
-    // }, []);
+    useEffect(() => {
+        console.log('Modified');
+    }, []);
     return (
         <div className="main-container">
             <BrowserRouter>
